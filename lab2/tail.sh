75005a961a791540f80528651a4f2eb8027d7732
Sergey Dmitrievich <serj_1313@mail.ru>
2024-04-03 01:34:22 +0300

    lab2: выделены предполагаемые функции библиотеки

--- lab2/tail.sh
+++ lab2/tail.sh
@@ -1,5 +1,3 @@
-rlc.bat tail.ref -o a.exe
-./a.exe -2 First.ref tail.ref tail.sh
-echo "----------"
-./a.exe First.ref tail.ref tail.sh
+rlc.bat tail.ref LibraryEx.ref -o a.exe
+./a.exe $*
 rm a.exe
