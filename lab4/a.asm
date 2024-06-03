b2d7abd02cad7053005bc748c833b6618585e0c7
Sergey Dmitrievich <serj_1313@mail.ru>
2024-06-03 19:10:32 +0300

    lab4: проверка типов

--- lab4/a.asm
+++ lab4/a.asm
@@ -61,7 +61,7 @@ GETFP SETSP SETFP
 1 RETN 
 
 :_Animal_vtbl__ 
-_Animal__setName _Animal__getName _Animal__getType 
+0 _Animal__setName _Animal__getName _Animal__getType 
 
 :_Cat__setBreed 
 GETFP GETSP SETFP 
@@ -85,7 +85,7 @@ GETFP SETSP SETFP
 1 RETN 
 
 :_Cat_vtbl__ 
-_Animal__setName _Animal__getName _Cat__getType _Cat__setBreed _Cat__getBreed 
+_Animal_vtbl__ _Animal__setName _Animal__getName _Cat__getType _Cat__setBreed _Cat__getBreed 
 
 :_main 
 GETFP GETSP SETFP 
@@ -96,48 +96,196 @@ GETFP 5 SUB _Cat_vtbl__ SAVE
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 2 SUB SAVE 
-1501 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 0 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+1501 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 0 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 2 SUB SAVE 
-GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 1 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV _printInt CALL GETRV DROP 
 _newLine CALL GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 2 SUB SAVE 
-GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 2 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 2 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV _printInt CALL GETRV DROP 
 _newLine CALL GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
-100500 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 0 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+100500 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 0 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
-1007 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 3 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+1007 GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 3 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
-GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 1 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV _printInt CALL GETRV DROP 
 _newLine CALL GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
-GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 4 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 4 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV _printInt CALL GETRV DROP 
 _newLine CALL GETRV DROP 
 GETFP GETSP SETFP 
 1 PUSHN 
 GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
-GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 2 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
+GETFP 1 SUB LOAD GETFP 1 SUB LOAD LOAD 1 ADD 2 ADD LOAD CALL GETRV SETRV GETFP SETSP SETFP 
 GETRV _printInt CALL GETRV DROP 
 _newLine CALL GETRV DROP 
+GETFP GETSP SETFP 
+2 PUSHN 
+GETFP 1 SUB GETFP LOAD 2 SUB SAVE 
+:_loop_while_0_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _bool_0_main JNE 
+_exit_while_0_main JMP 
+:_bool_0_main 
+GETFP 1 SUB LOAD 
+_Animal_vtbl__ 
+CMP _true_while_0_main JNE 
+_exit_while_0_main JMP 
+
+:_true_while_0_main 
+GETFP 1 SUB GETFP 1 SUB LOAD LOAD SAVE 
+_loop_while_0_main JMP 
+:_exit_while_0_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _true_if_1_main JNE 
+_false_if_1_main JMP 
+:_true_if_1_main 
+GETFP 2 SUB 1 SAVE 
+_exit_if_1_main JMP 
+:_false_if_1_main 
+GETFP 2 SUB 0 SAVE 
+:_exit_if_1_main 
+GETFP 2 SUB LOAD SETRV GETFP SETSP SETFP 
+GETRV 
+1 
+CMP _exit_if_0_main JEQ 
+_true_if_0_main JMP 
+:_true_if_0_main 
+6661 _printInt CALL GETRV DROP 
+_newLine CALL GETRV DROP 
+:_exit_if_0_main 
+GETFP GETSP SETFP 
+2 PUSHN 
+GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
+:_loop_while_1_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _bool_1_main JNE 
+_exit_while_1_main JMP 
+:_bool_1_main 
+GETFP 1 SUB LOAD 
+_Animal_vtbl__ 
+CMP _true_while_1_main JNE 
+_exit_while_1_main JMP 
+
+:_true_while_1_main 
+GETFP 1 SUB GETFP 1 SUB LOAD LOAD SAVE 
+_loop_while_1_main JMP 
+:_exit_while_1_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _true_if_3_main JNE 
+_false_if_3_main JMP 
+:_true_if_3_main 
+GETFP 2 SUB 1 SAVE 
+_exit_if_3_main JMP 
+:_false_if_3_main 
+GETFP 2 SUB 0 SAVE 
+:_exit_if_3_main 
+GETFP 2 SUB LOAD SETRV GETFP SETSP SETFP 
+GETRV 
+1 
+CMP _true_if_2_main JEQ 
+_exit_if_2_main JMP 
+:_true_if_2_main 
+6662 _printInt CALL GETRV DROP 
+_newLine CALL GETRV DROP 
+:_exit_if_2_main 
+GETFP GETSP SETFP 
+2 PUSHN 
+GETFP 1 SUB GETFP LOAD 2 SUB SAVE 
+:_loop_while_2_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _bool_2_main JNE 
+_exit_while_2_main JMP 
+:_bool_2_main 
+GETFP 1 SUB LOAD 
+_Cat_vtbl__ 
+CMP _true_while_2_main JNE 
+_exit_while_2_main JMP 
+
+:_true_while_2_main 
+GETFP 1 SUB GETFP 1 SUB LOAD LOAD SAVE 
+_loop_while_2_main JMP 
+:_exit_while_2_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _true_if_5_main JNE 
+_false_if_5_main JMP 
+:_true_if_5_main 
+GETFP 2 SUB 1 SAVE 
+_exit_if_5_main JMP 
+:_false_if_5_main 
+GETFP 2 SUB 0 SAVE 
+:_exit_if_5_main 
+GETFP 2 SUB LOAD SETRV GETFP SETSP SETFP 
+GETRV 
+1 
+CMP _true_if_4_main JEQ 
+_exit_if_4_main JMP 
+:_true_if_4_main 
+6663 _printInt CALL GETRV DROP 
+_newLine CALL GETRV DROP 
+:_exit_if_4_main 
+GETFP GETSP SETFP 
+2 PUSHN 
+GETFP 1 SUB GETFP LOAD 5 SUB SAVE 
+:_loop_while_3_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _bool_3_main JNE 
+_exit_while_3_main JMP 
+:_bool_3_main 
+GETFP 1 SUB LOAD 
+_Cat_vtbl__ 
+CMP _true_while_3_main JNE 
+_exit_while_3_main JMP 
+
+:_true_while_3_main 
+GETFP 1 SUB GETFP 1 SUB LOAD LOAD SAVE 
+_loop_while_3_main JMP 
+:_exit_while_3_main 
+GETFP 1 SUB LOAD 
+0 
+CMP _true_if_7_main JNE 
+_false_if_7_main JMP 
+:_true_if_7_main 
+GETFP 2 SUB 1 SAVE 
+_exit_if_7_main JMP 
+:_false_if_7_main 
+GETFP 2 SUB 0 SAVE 
+:_exit_if_7_main 
+GETFP 2 SUB LOAD SETRV GETFP SETSP SETFP 
+GETRV 
+1 
+CMP _exit_if_6_main JEQ 
+_true_if_6_main JMP 
+:_true_if_6_main 
+6664 _printInt CALL GETRV DROP 
+_newLine CALL GETRV DROP 
+:_exit_if_6_main 
 GETFP SETSP SETFP 
 0 SETRV __main JMP 
 :__main 
