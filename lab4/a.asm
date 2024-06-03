cb0d12c557d53c350217f2629a2be2e9a6abd59d
Sergey Dmitrievich <serj_1313@mail.ru>
2024-06-03 14:48:26 +0300

    lab4: компиляция вызовов методов

--- lab4/a.asm
+++ lab4/a.asm
@@ -1,1 +1,1 @@
-(function main ()(block ((point 2 ))(point = Point2_vtbl__ )(call printInt 0 ))(return 0 ))
+(function main ()(block ((point 2 ))(point = Point2_vtbl__ )(call printInt (let ((object_let__ 1 ))(object_let__ = point )(call (L ((L (L object_let__ ))+ getX ))(L object_let__ )))))(return 0 ))
